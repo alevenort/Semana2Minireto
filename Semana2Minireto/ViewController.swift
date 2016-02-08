7651d3e3922eb4fb2edefd361713f5cfaa278ba0
Alejandro Venegas Ortiz <alejo43200@gmail.com>
2016-02-08 15:13:36 -0500

    1

--- Semana2Minireto/ViewController.swift
+++ Semana2Minireto/ViewController.swift
@@ -19,7 +19,32 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+     
+     //: minireto
+     
+     import UIKit
+     
+     for i in 0...100
+     {
+     if i % 5 == 0 && i >= 30 && i <= 40 {
+     print ( i, "Viva Swift !!!", "Bingo")
+     } else if i >= 30 && i <= 40 && i % 2 == 0 {
+     print ( i, "Viva Swift !!!", "Par" )
+     } else if i % 2 == 0 && i % 5 == 0 {
+     print ( i, "Par", "Bingo" )
+     } else if i % 2 == 1 && i % 5 == 0 {
+     print ( i, "Impar", "Bingo" )
+     } else if i >= 30 && i <= 40 {
+     print ( i, "Viva Swift !!!", "Impar" )
+     } else if i % 2 == 0 {
+     print ( i, "Par")
+     } else if i % 2 == 1 {
+     print ( i, "Impar")
+     }
+     }
+     
+    
 }
 
